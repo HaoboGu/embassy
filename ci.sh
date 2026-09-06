1a3a37d01a8871e005bdfd2389f3877352fffda6
xoviat <xoviat@users.noreply.github.com>
2026-09-06 09:16:30 -0500

    ci: disable failing boards

--- ci.sh
+++ ci.sh
@@ -49,13 +49,15 @@ rm -rf out/tests/mspm0g3507
 
 # temporarily disabled
 rm -rf  out/tests/stm32wba65ri
+rm -rf  out/tests/stm32l152re
+rm -rf  out/tests/stm32f207zg
 rm -rf  out/tests/nrf9160-dk
 
 # unstable, I think it's running out of RAM?
-rm out/tests/stm32f207zg/eth
+# rm out/tests/stm32f207zg/eth
 
 # temporarily disabled, flaky.
-rm out/tests/stm32f207zg/usart_rx_ringbuffered
+# rm out/tests/stm32f207zg/usart_rx_ringbuffered
 rm out/tests/stm32l152re/usart_rx_ringbuffered
 
 # doesn't work, gives "noise error", no idea why. usart_dma does pass.
